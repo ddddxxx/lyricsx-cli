7e37df6f4a810732483f5138593c0f0277135040
sea <simpleslight@icloud.com>
2021-06-29 16:19:45 +0800

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
         .package(url: "https://github.com/ddddxxx/LyricsKit", .upToNextMinor(from: "0.9.1")),
-        .package(url: "https://github.com/cx-org/CXExtensions", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/cx-org/CXExtensions", .upToNextMinor(from: "0.3.1")),
         .package(url: "https://github.com/suransea/MusicPlayer", .branch("master")),
     ],
     targets:  [
